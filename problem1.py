ce8a52157d04a2830b257c29c6d5675b16fd2068
Veena-121 <vcode8963@gmail.com>
2025-06-07 17:53:55 +0530

    1

--- problem1.py
+++ problem1.py
@@ -1,1 +1,14 @@
-user= input
+# Odd or Even Checker
+
+def main():
+    num = int(input("Enter a number: "))
+    
+    if (num%2 ==0):
+        print("Even")
+    else:
+        print("Odd")
+        
+
+if __name__ == "__main__":
+    main()
+    
